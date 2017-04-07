0460636d8eb2c31e66636a51616b760ff57fb4bb
gaoyihan <ygao34@siebl-1119-01.ad.uillinois.edu>
2017-04-07 14:39:43 -0500

    first commit

---
+++ base.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <vector>
+
+struct Matrix {
+    int m, n;
+    std::vector<double> val;
+    Matrix(int m_, int n_) : m(m_), n(n_) {}
+    double& At(int i, int j) { return val[i * m + j]; }
+};
+
+struct Graph {
+    int size;
+    std::vector<std::vector<int>> edge;
+    Graph(int size_) : size(size_) {
+        edge.resize(size);
+    }
+    void AddEdge(int x, int y) {
+        edge[x].push_back(y);
+        edge[y].push_back(x);
+    }
+    const std::vector<int>& Adjacent(int x) { return edge[x]; }
+};
+
+struct Edge {
+    int x, y;
+    Edge(int x_, int y_) : x(x_), y(y_) {}
+};
+
+class Model {
+  public:
+    Model() {}
+    virtual ~Model() {}
+    virtual void Train(const Graph& graph) = 0;
+    virtual double Evaluate(int x, int y) = 0;
+};
+
+void ReadDataset(const std::string& filename, Graph* graph);
+
